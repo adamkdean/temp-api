db4faa09ec528029116b4279ed0919fce7c494e1
Adam K Dean <adam@amberfish.co.uk>
2014-12-15 19:36:52 +0000

    Added restify and sample API code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(process.env);
+var restify = require('restify');
+
+var server = new restify.createServer();
+
+server.get('/', function(req, res, next) {
+    console.log('hello world');
+});
+
+server.listen(process.env.PORT, function() {
+    console.log('%s listening at %s', server.name, server.url);
+})
