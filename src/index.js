f9b411421b00e8dac47e847f22c08c4b1903664e
Adam K Dean <adam@amberfish.co.uk>
2014-12-15 19:42:51 +0000

    Added env response

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var restify = require('restify');
 var server = new restify.createServer();
 
 server.get('/', function(req, res, next) {
-    res.send('hello world');
+    res.send(process.env);
 });
 
 server.listen(process.env.PORT, function() {
