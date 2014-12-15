759d67fabd0537332a29a7a2ca310134b53bf3dd
Adam K Dean <adam@amberfish.co.uk>
2014-12-15 19:41:07 +0000

    Added hello world response properly

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var restify = require('restify');
 var server = new restify.createServer();
 
 server.get('/', function(req, res, next) {
-    req.send('hello world');
+    res.send('hello world');
 });
 
 server.listen(process.env.PORT, function() {
