b48a322034f4311793aa72ceeb87f9239c19329c
Adam K Dean <adam@amberfish.co.uk>
2014-12-15 20:33:28 +0000

    Added test db writing script in

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-var restify = require('restify');
+var restify = require('restify'),
+    moment = require('moment');
 
-var server = new restify.createServer();
+var server = new restify.createServer(),
+    connectionString = proccess.env.DATABASE_URL || throw 'no database URL',
+    client, query, sql;
+
 server.use(restify.bodyParser());
 
 server.get('/', function(req, res, next) {
@@ -9,20 +13,24 @@ server.get('/', function(req, res, next) {
 
 server.post('/submit', function(req, res, next) {
     var api_key = req.body.api_key,
-        t1 = req.body.t1,
-        t2 = req.body.t2;
-
-    console.log('process.env.API_KEY: %s', process.env.API_KEY);
-    console.log('api_key: %s', api_key);
+        t1 = req.body.t1 || 0,
+        t2 = req.body.t2 || 0,
+        datetime = moment().format('YYYY-MM-DD HH:mm:ss');
 
     if (process.env.API_KEY === undefined || api_key != process.env.API_KEY) {
-        res.send('403 FORBIDDEN', process.env.API_KEY === undefined, api_key != process.env.API_KEY);
+        res.send('403 FORBIDDEN');
         return;
     }
 
-    if ()
+    sql = "INSERT INTO temperature(datetime, t1, t2) VALUES ('" + datetime + "', '" + t1 + "', '" + t2 + "')";
+    client = new pg.Client(connectionString);
+    client.connect();
+    query = client.query(sql);
+    query.on('end', function() {
+        client.end();
 
-    res.send('200 OK - recording ' + t1 + ' & ' + t2);
+        res.send('200 OK - recorded ' + t1 + ', ' + t2);
+    })
 });
 
 server.listen(process.env.PORT, function() {
